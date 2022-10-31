8ea9db65903c25ddfbe44eacf79f29e3c3eef719
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-31 19:20:23 +0100

    Initial Commit

---
+++ Names to Faces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Names to Faces
+//
+//  Created by Melis Yazıcı on 31.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
