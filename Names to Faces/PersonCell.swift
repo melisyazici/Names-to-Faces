ee86da600da4a17ae7b255cf485e080a8b4905d5
Melis Yazıcı <melis.yazici95@icloud.com>
2022-10-31 20:11:58 +0100

    Design UICollectionView cells

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Melis Yazıcı on 31.10.22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
